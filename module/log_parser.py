efb3c4dc10a3477cbc99b8b3f2829df86da05224
Imane Benbouziane <imane.benbouziane@universite-paris-saclay.fr>
2025-12-18 14:37:05 +0100

    log-parser ajout dictionnaire

--- module/log_parser.py
+++ module/log_parser.py
@@ -3,20 +3,21 @@ import re
 def extract_ip_authlog(log_content):
     
     # Expression régulière pour les adresses IP
-    ip_address = re.compile(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')
+    ip_address = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
     
     # Expressions régulières pour les erreurs spécifiques
-    failed_password = re.compile(r'echec d’authentification')
+    failed_password = re.compile(r"echec d'authentification")
     
-    # Liste pour stocker les adresses IP 
-    ip_addresses_SSH = []
+    # Dictionnaire pour stocker les adresses IP et leur occurences
+    ip_addresses_SSH = {}
     
     # Parcourir chaque ligne du contenu du log et extraire les adresses IP
     for line in log_content.splitlines():
         # cas des échecs d'authentification SSH
         if failed_password.search(line):
             ips = ip_address.findall(line)
-            ip_addresses_SSH.append(ips)
+            for ip in ips:
+                ip_addresses_SSH[ip] = ip_addresses_SSH.get(ip, 0) + 1
     
     return {
         'failed_passwords': list(ip_addresses_SSH)
@@ -25,20 +26,22 @@ def extract_ip_authlog(log_content):
 def extract_ip_accesslog(log_content):
     
     # Expression régulière pour les adresses IP
-    ip_address = re.compile(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')
+    ip_address = re.compile(r'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
     
-    # Expressions régulières pour les erreurs spécifiques
-    error = re.compile(r'404|bot|crawl|spider|Not Found', re.IGNORECASE)
+    # Expressions régulières pour les erreurs 404 et les bots
+    error_404 = re.compile(r'404')   
+    bot_user_agents = re.compile(r'bot|crawl|spider|slurp', re.IGNORECASE)
     
-    # Liste pour stocker les adresses IP 
-    ip_addresses_HTTP = []
+    # Dictionnaire pour stocker les adresses IP et leur occurences
+    ip_addresses_HTTP = {}
     
     # Parcourir chaque ligne du contenu du log et extraire les adresses IP
     for line in log_content.splitlines():
         # cas des erreurs 404 ou des bots
-        if error.search(line) :
+        if error_404.search(line) or bot_user_agents.search(line):
             ips = ip_address.findall(line)
-            ip_addresses_HTTP.append(ips)
+            for ip in ips:
+                ip_addresses_HTTP[ip] = ip_addresses_HTTP.get(ip, 0) + 1
     
     return {
         'http_errors': list(ip_addresses_HTTP)
